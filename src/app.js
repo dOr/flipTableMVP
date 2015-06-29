678cbad1a9aa7a6f0f260bf884727c5fa116597f
chikeichan <chikeichan@gmail.com>
2015-06-28 23:19:23 -0700

    fixed

--- src/app.js
+++ src/app.js
@@ -4,13 +4,14 @@ socket.on('connect', function(d) {
     console.log('Connected to server...');
 });
 
+var dBroadcastMovement = _.debounce(broadcastMovement, 100);
+
 if (window.DeviceOrientationEvent) {
     window.addEventListener("deviceorientation", function(event) {
         dBroadcastMovement(event);
     }, true);
 }
 
-var dBroadcastMovement = _.debounce(broadcastMovement, 100);
 
 function broadcastMovement(d) {
     socket.emit('dOr', d);
@@ -20,6 +21,8 @@ socket.on('broadcast', function(d) {
     var alpha = document.getElementById('alpha');
     var gamma = document.getElementById('gamma');
     var beta = document.getElementById('beta');
+
+    console.log(d);
 
     alpha.innerText = d.alpha;
     gamma.innerText = d.gamma;
