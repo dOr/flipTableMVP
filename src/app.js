bc6d33bc59649c62c5cd54beca32d6567d22d449
chikeichan <chikeichan@gmail.com>
2015-06-28 23:53:18 -0700

    no sigfig

--- src/app.js
+++ src/app.js
@@ -1,23 +1,49 @@
+var isStale = true;
 
+var pDor = {
+    alpha: null,
+    gamma: null,
+    beta: null
+}
 
+var dor = {
+    alpha: null,
+    gamma: null,
+    beta: null
+}
 
+setTimeout(function() {
+    var diff = dor.gamma - pDor.gamme;
+    var diffEl = document.getElementById('diff');
+
+    diffEl.innerText = Math.floor(diff);
+
+    if(diff > 1) {
+        isStale = false;
+    }
+}, 50)
 
 
 if (window.DeviceOrientationEvent) {
     window.addEventListener("deviceorientation", function(event) {
 
         // alpha: rotation around z-axis
-               var rotateDegrees = event.alpha;
-               // gamma: left to right
-               var leftToRight = event.gamma;
-               // beta: front back motion
-               var frontToBack = event.beta;
+        // gamma: left to right
+        // beta: front back motion
+         
+        pDor = {
+            alpha: dor.alpha,
+            gamma: dor.gamma,
+            beta: dor.beta
+        }
 
-        broadcastMovement({
+        dor = {
             alpha: event.alpha,
             gamma: event.gamma,
             beta: event.beta
-        });
+        };
+
+        broadcastMovement(dor);
     }, true);
 }
 
@@ -30,5 +56,6 @@ function broadcastMovement(d) {
         alpha.innerText = Math.floor(d.alpha);
         gamma.innerText = Math.floor(d.gamma);
         beta.innerText = Math.floor(d.beta);
+        
 };
 
