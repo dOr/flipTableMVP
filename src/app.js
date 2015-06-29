a98d844901a6ec0c48a64fccb5496fb989f574a3
chikeichan <chikeichan@gmail.com>
2015-06-28 23:22:12 -0700

    fixed

--- src/app.js
+++ src/app.js
@@ -4,19 +4,6 @@ socket.on('connect', function(d) {
     console.log('Connected to server...');
 });
 
-var dBroadcastMovement = _.debounce(broadcastMovement, 100);
-
-if (window.DeviceOrientationEvent) {
-    window.addEventListener("deviceorientation", function(event) {
-        dBroadcastMovement(event);
-    }, true);
-}
-
-
-function broadcastMovement(d) {
-    socket.emit('dOr', d);
-};
-
 socket.on('broadcast', function(d) {
     var alpha = document.getElementById('alpha');
     var gamma = document.getElementById('gamma');
@@ -27,4 +14,29 @@ socket.on('broadcast', function(d) {
     alpha.innerText = d.alpha;
     gamma.innerText = d.gamma;
     beta.innerText = d.beta;
-})
+});
+
+var dBroadcastMovement = _.debounce(broadcastMovement, 100);
+
+if (window.DeviceOrientationEvent) {
+    window.addEventListener("deviceorientation", function(event) {
+
+        // alpha: rotation around z-axis
+               var rotateDegrees = event.alpha;
+               // gamma: left to right
+               var leftToRight = event.gamma;
+               // beta: front back motion
+               var frontToBack = event.beta;
+
+        dBroadcastMovement({
+            alpha: event.alpha,
+            gamma: event.gamma,
+            beta: event.beta
+        });
+    }, true);
+}
+
+
+function broadcastMovement(d) {
+    socket.emit('dOr', d);
+};
